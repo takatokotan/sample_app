fb3e0fb4653e7cf9d3cbcb8ab3867e8b9cb87222
takatokotan <hiyori0701@yahoo.co.jp>
2020-04-12 22:51:22 +0900

    Finish user edit, update, index, and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
-  test "layout links" do
+  test "layout links when not logged in" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
@@ -10,7 +10,35 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", login_path
     get contact_path
     assert_select "title", full_title("Contact")
+    get signup_path
+    # 特定のHTMLタグが存在する　タグの種類（title）, タイトルの文字（full_titleヘルパーを呼び出し）
+    assert_select "title", full_title("Sign up")
+  end
+
+   # 以下のテスト直前に@userにusers(:michael)を代入
+   def setup
+    @user = users(:michael)
+  end
+  
+  #ログインしている場合のテスト
+  test "layout links when logged in" do
+    # ログインする
+    log_in_as(@user)
+    # root_pathへgetのリクエスト
+    get root_path
+    # static_pages/homeが描画される
+    assert_template 'static_pages/home'
+    # 特定のHTMLタグが存在する　タグの種類(a href), リンク先のパス, タグの数
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user) 
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
   end
 end
